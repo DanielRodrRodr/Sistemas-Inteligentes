64eac2547aeabb58d03779c8d723f9c601d4d4b1
DanielRodrRodr <146079148+DanielRodrRodr@users.noreply.github.com>
2026-03-16 10:03:46 +0100

    Update ReactiveAgent

--- Practica2/BattleCityReactiveAgentPG/Reactive/ReactiveAgent.py
+++ Practica2/BattleCityReactiveAgentPG/Reactive/ReactiveAgent.py
@@ -4,6 +4,8 @@ from States.DodgeBullet import DodgeBullet
 from States.GoToCommandCenter import GoToCommandCenter
 from States.OrientateAndShoot import OrientateAndShoot
 from States.GoToExit import GoToExit
+from States.AgentConsts import AgentConsts
+import random
 
 
 class ReactiveAgent(BaseAgent):
@@ -17,6 +19,10 @@ class ReactiveAgent(BaseAgent):
         }
         self.stateMachine = StateMachine("ReactiveBehavior",dictionary,"GoToCommandCenter")
 
+        self.last_x = None
+        self.last_y = None
+        self.ciclos_atascado = 0
+
     #Metodo que se llama al iniciar el agente. No devuelve nada y sirve para contruir el agente
     def Start(self):
         print("Inicio del agente ")
@@ -26,6 +32,30 @@ class ReactiveAgent(BaseAgent):
     #Devuelve la acción u el disparo si o no
     def Update(self, perception, map):
         action, shot = self.stateMachine.Update(perception, map, self)
+
+        x = perception[AgentConsts.AGENT_X]
+        y = perception[AgentConsts.AGENT_Y]
+
+        #Comprobar si se queda atascado el agente
+        if self.last_x is not None and self.last_y is not None:
+            if abs(x - self.last_x) < 0.05 and abs(y - self.last_y) < 0.05:
+                self.ciclos_atascado += 1
+            else:
+                self.ciclos_atascado = 0
+
+        self.last_x = x
+        self.last_y = y
+
+        # Si se queda atascado varios ciclos se mueve
+        if self.ciclos_atascado >= 2:
+            action = random.choice([
+                AgentConsts.MOVE_LEFT,
+                AgentConsts.MOVE_RIGHT,
+                AgentConsts.MOVE_UP,
+                AgentConsts.MOVE_DOWN
+            ])
+            self.ciclos_atascado = 0
+
         return action, shot
     
     #Metodo que se llama al finalizar el agente, se pasa el estado de terminacion
