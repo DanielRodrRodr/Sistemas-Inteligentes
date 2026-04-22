848bd26375e766354b46af4a3a792d4bb346debe
alvaroturi11 <alvarotu@ucm.es>
2026-04-22 17:53:52 +0200

    MainCompetitive eliminado

--- Practica3_multiagente/MainDeliverativeAgent.py
+++ Practica3_multiagente/MainDeliverativeAgent.py
@@ -2,10 +2,14 @@ import sys
 sys.path.insert(0,"./LGym")
 sys.path.insert(0,"./Agent")
 sys.path.insert(0,"./Deliverative")
+sys.path.insert(0,"./Competitive")
 from LGym.LGymClient import agentLoop
 from Agent.BaseAgent import BaseAgent
 from Deliverative.GoalOrientedAgent import GoalOrientedAgent
+from Competitive.CompetitiveAgent import CompetitiveAgent
 
 
-agent = GoalOrientedAgent("1","MyAgent")
-agentLoop(agent,True)
+agent1 = GoalOrientedAgent("1","MyAgent")
+agentLoop(agent1,True)
+agent2 = CompetitiveAgent("1","CompetitiveAgent")
+agentLoop(agent2,True)
